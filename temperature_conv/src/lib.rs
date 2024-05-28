1ce843b706f3f8e5f63ceecc6a22494976ecc56b
daiba <badaibou5@gmail.com>
2024-05-28 00:53:25 +0000

    temperature conv commit

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,8 +1,9 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-
-(f - 32) * (9/5)
+    (f - 32.0) / (9.0 / 5.0)
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    (c * (9/5) + 32)
+    (c * (9.0 / 5.0)) + 32.0
 }
+
+    
