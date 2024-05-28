dcef0201471fd9a219ff886af83e2faaf97c7651
daiba <badaibou@gmail.com>
2024-05-28 05:35:23 +0000

    looping commit

---
+++ tuples_refs/src/lib.rs
@@ -0,0 +1,17 @@
+#[derive(Debug,Clone)]
+pub struct Student(i32, String, String);
+
+pub fn id(student: &Student) -> i32 {
+    student.0
+}
+
+pub fn first_name(student: &Student) -> String {
+    let first=student.1.clone();
+    first
+}
+
+pub fn last_name(student: &Student) -> String {
+    let last=student.2.clone();
+    last
+}
+
