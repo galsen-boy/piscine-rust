57c1b543cf4199beba180860fdec73f3e021a74f
daiba <badaibou@gmail.com>
2024-05-28 05:02:48 +0000

    looping commit

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,29 +1,23 @@
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
-    let mut count = 1;
-    loop {  
-        println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
-        let mut anwser = String::new();
-        io::stdin()
-            .read_line(&mut anwser)
-            .expect("Failed to read line");
-        let anwser: String = match anwser.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        
-        if anwser == "The letter e" {
-            if count == 1 {
-                println!("It took you {} trial to get the right answer", count);
-                break;
-            } else {
-                println!("It took you {} trials to get the right answer", count);
-                break;
-            }
-        } else {
-          count += 1;
+    let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
+    let answer = "The letter e";
+    let mut tries = 0;
+
+    loop {
+        println!("{}", riddle);
+        io::stdout().flush().expect("Failed to flush stdout.");
+
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).expect("Failed to read line.");
+
+        tries += 1;
+
+        if user_input.trim() == answer {
+            break;
         }
-      
-    }; 
+    }
+
+    println!("Number of trials: {}", tries);
 }
