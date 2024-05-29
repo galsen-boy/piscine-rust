131aa01474c6e4d95916e8977625ab783bfe2f45
daiba <badaibou@gmail.com>
2024-05-29 00:16:54 +0000

    quest 2 first exercises commit

--- to_url/src/lib.rs
+++ to_url/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn to_url(s: &str) -> String {
     let mut url = String::new();
     for i in s.chars() {
-        if i = ' ' {
+        if i == ' ' {
             url.push_str("%20");
         }else {
             url.push_str(i);
