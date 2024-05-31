86dbe8e07f57aeb12f648e7aa541060fe4fc4679
daiba <badaibou@gmail.com>
2024-05-31 05:34:38 +0000

    bigger commit

---
+++ is_anagram/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn is_anagram(s1: &str, s2: &str) -> bool {
+
+    let mut s1_chars: Vec<char> = s1.to_lowercase()chars().collect();
+    let mut s2_chars: Vec<char> = s2.to_lowercase()chars().collect();
+
+    s1_chars.sort();
+    s2_chars.sort();
+
+    s1_chars == s2_chars
+}
