876a4b232e9b5c283afaab9661e9480d3e2bb36f
daiba <badaibou@gmail.com>
2024-05-31 00:50:03 +0000

    borrow me the reference commit

---
+++ circle/src/lib.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+pub struct Circle {
+	pub center: Point,
+	pub radius: f64,
+}
+
+impl Circle {
+    pub fn new(center: Point, radius: f64) -> Self {
+        Circle {
+            center: center,
+            radius: radius,
+        }
+    }
+    pub fn area(&self) -> f64 {
+        std::f64::consts::PI * self.radius * self.radius
+    }
+    pub fn diameter(&self) -> {
+        self.radius * 2.0
+    }
+}
+
+#[derive(Debug)]
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl Point {
+    pub fn distance(&self, second_point: &Point) -> f64 {
+        ((second_point.x - self.x).powf(2.0) + (second_point.y - self.y).powf(2.0)).sqrt()
+    }
+}
