6412eeb3345322b2e78156e45b461894a3aaf7ea
daiba <badaibou@gmail.com>
2024-06-07 00:36:29 +0000

    mobs fucking I'm tired commit

---
+++ scores/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn score(s: &str) -> u64 {
+    let up = s.to_uppercase();
+    up.chars()
+        .map(|c| match c {
+            'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+            'D' | 'G' => 2,
+            'B' | 'C' | 'M' | 'P' => 3,
+            'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+            'K' => 5,
+            'J' | 'X' => 8,
+            'Q' | 'Z' => 10,
+            _ => 0,
+        })
+        .sum()
+}
