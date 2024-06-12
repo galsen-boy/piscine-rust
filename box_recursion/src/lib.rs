8916591ece2185e1efb027b7d889ffaff7be053a
daiba <badaibou@gmail.com>
2024-06-12 22:20:13 +0000

    pointers quest commit

--- box_recursion/src/lib.rs
+++ box_recursion/src/lib.rs
@@ -1,44 +1,41 @@
-use std::mem;
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct WorkEnvironment {
     pub grade: Link,
 }
-pub type Link = Option<Box<Worker>>;
+
+pub type Link =Option<Box<Worker>>;
+
 #[derive(Debug)]
 pub struct Worker {
-    pub worker_type: String,
-    pub worker_name: String,
-    pub next_worker: Link,
+    pub role: String,
+    pub name: String,
+    pub next: Link,
 }
-impl Worker {
-    fn new(t: String, name: String) -> Self {
-        Self {
-            worker_type: t,
-            worker_name: name,
-            next_worker: None,
-        }
-    }
-}
+
 impl WorkEnvironment {
-    pub fn new() -> Self {
-        Self {
-            grade: None,
+    pub fn new() -> WorkEnvironment {
+        WorkEnvironment{
+            grade:None
         }
     }
-    pub fn add_worker(&mut self, t: String, name: String) {
-        let old = mem::replace(
-            &mut self.grade,
-            Some(Box::new(Worker::new(t, name)))
-        );
-        self.grade.as_mut().unwrap().next_worker = old
+    pub fn add_worker(&mut self, role: String, name: String) {
+        let worker = Worker {
+            role,
+            name,
+            next: self.grade.take(),
+        };
+        self.grade = Some(Box::new(worker));
     }
     pub fn remove_worker(&mut self) -> Option<String> {
-        self.grade.take().map(|mut worker| {
-            self.grade = worker.next_worker.take();
-            worker.worker_name
-        })
+        if let Some(mut boxed_worker) = self.grade.take() {
+            let worker_name = boxed_worker.name.clone();
+            self.grade = boxed_worker.next.take();
+            Some(worker_name)
+        } else {
+            None
+        }
     }
     pub fn last_worker(&self) -> Option<(String, String)> {
-        self.grade.as_ref().map(|worker| (worker.worker_name.clone(), worker.worker_type.clone()))
+        self.grade.as_ref().map(|worker| (worker.name.clone(), worker.role.clone()))
     }
 }
