07ddbbc03e147b979340a6dfcaf2e96d38107d2b
daiba <badaibou@gmail.com>
2024-06-20 04:21:08 +0000

    last quest commit

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -4,18 +4,19 @@ pub struct Collatz {
 }
 
 impl Iterator for Collatz {
-    type Item = Collatz;
+    type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.v == 1 {
             None
         } else {
+            let current = self.v;
             self.v = if self.v % 2 == 0 {
                 self.v / 2
             } else {
-                3 * self.v + 1
+                self.v * 3 + 1
             };
-            Some(Collatz { v: self.v })
+            Some(current)
         }
     }
 }
@@ -27,10 +28,5 @@ impl Collatz {
 }
 
 pub fn collatz(n: u64) -> usize {
-    let mut collatz = Collatz::new(n);
-    let mut steps = 0;
-    while let Some(_) = collatz.next() {
-        steps += 1;
-    }
-    steps
+    Collatz::new(n).count()
 }
