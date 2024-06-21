9e3ecf332eeea0709d5e9d50652675084ae76b5e
DAIBOU BA <daiba@noreply.learn.zone01dakar.sn>
2024-06-21 16:43:48 +0000

    Update 'iterators/src/lib.rs'

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -1,32 +1,29 @@
-#[derive(Copy, Clone, Debug)]
+	#[derive(Copy, Clone, Debug)]
 pub struct Collatz {
     pub v: u64,
 }
-
 impl Iterator for Collatz {
-    type Item = u64;
-
+    type Item = Collatz;
     fn next(&mut self) -> Option<Self::Item> {
-        if self.v == 1 {
-            None
-        } else {
-            let current = self;
-            self.v = if self.v % 2 == 0 {
-                self.v / 2
-            } else {
-                self.v * 3 + 1
-            };
-            Some(current)
+        let current_value = *self;
+        if self.v <= 1 {
+            return None;
         }
+        if self.v % 2 == 0 {
+            self.v /= 2;
+        } else {
+            self.v = self.v * 3 + 1;
+        }
+        Some(current_value)
     }
 }
-
 impl Collatz {
-    pub fn new(n: u64) -> Self {
+	pub fn new(n: u64) -> Self {
         Collatz { v: n }
     }
 }
-
 pub fn collatz(n: u64) -> usize {
     Collatz::new(n).count()
+    
 }
+
