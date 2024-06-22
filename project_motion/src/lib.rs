b5e825a891d8d521f081a877a9312b647a0e7a1d
daiba <badaibou5@gmail.com>
2024-06-22 09:26:50 +0000

    project motion commit

--- project_motion/src/lib.rs
+++ project_motion/src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Object {
+    pub x: f32,
+    pub y: f32,
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct ThrowObject {
+    pub init_position: Object,
+    pub init_velocity: Object,
+    pub actual_position: Object,
+    pub actual_velocity: Object,
+    pub time: f32,
+}
+impl ThrowObject {
+    pub fn new(init_position: Object, init_velocity: Object) -> ThrowObject {
+        ThrowObject { init_position, init_velocity, actual_position: init_position, actual_velocity: init_velocity , time: 0. }
+    }
+}
+impl Iterator for ThrowObject {
+    type Item = Self;
+    fn next(&mut self) -> Option<Self::Item> {
+        // let next = *self;
+        
+        self.actual_position.y = ((self.actual_position.y + self.actual_velocity.y - (1./2.) * 9.8)*100.) .round() / 100.;
+        
+        self.actual_velocity.y = ((self.actual_velocity.y - 9.8)*100.) .round() / 100.;
+        
+        self.actual_position.x = ((self.actual_position.x + self.actual_velocity.x)*100.) .round() / 100.;
+        
+        self.actual_velocity.x = ((self.actual_velocity.x)*100.) .round() / 100.;
+        
+        self.time += 1.;
+        if self.actual_position.y <= 0. {
+            None
+        } else {
+            Some(self.clone())
+        }
     }
 }
